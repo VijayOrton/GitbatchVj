b769d9faca30b005815e41fe824c1ab679b2e90e
VijayOrton <sowjenvijay743@gmail.com>
2022-11-23 15:07:27 +0530

    vijay add code

--- src/gitMove/Git.java
+++ src/gitMove/Git.java
@@ -1,6 +1,15 @@
 package gitMove;
 
 public class Git {
+	
+	private void m1() {
+		System.out.println("vj update");
+
+	}
+	private void m2() {
+	 System.out.println("vj2 update");
+
+	}
 	public static void main(String[] args) {
 		
 	System.out.println("vj");
